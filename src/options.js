113d71e1ade281257e77a75ba89bdac578bb77d0
PND <fujjisaaan@gmail.com>
2019-09-10 11:13:17 +0900

    Use the popup for user preferences.

--- src/options.js
+++ src/options.js
@@ -23,22 +23,21 @@ const setStorageData = (key, value) => {
 
 // Save Settings
 document.querySelector('#save-button').addEventListener('click', async () => {
-  const inputData = document.querySelector('#keyword-list textArea').value;
+  const inputData = document.querySelector('#keyword-list').value;
   const keywords = inputData.split('\n').filter(x => x !== '');
   await setStorageData(storageKey, keywords);
-  document.querySelector('#result-dialog').showModal();
-});
-
-// Close Dialog
-document.querySelector('#result-dialog .close').addEventListener('click', () => {
-  document.querySelector('dialog').close();
+  const snackbar = document.querySelector('#save-notifier').MaterialSnackbar;
+  snackbar.showSnackbar({
+    message: '設定を保存しました',
+    timeout: 1000,
+  });
 });
 
 const init = async () => {
   const storageData = await getStorageData(storageKey);
   const keywordList = storageData[storageKey];
 
-  const textArea = document.querySelector('#keyword-list textArea');
+  const textArea = document.querySelector('#keyword-list');
   textArea.value = keywordList.join('\n');
 };
 
