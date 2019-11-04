19603c86cebc979ba2457ac36a76ac5bc79d2cda
PND <fujjisaaan@gmail.com>
2019-11-04 11:00:07 +0900

    Add an option to use regular expressions for the filter.

--- src/options.js
+++ src/options.js
@@ -1,4 +1,5 @@
 const storageKey = 'keywordList';
+const regexKey = 'isRegexEnabled';
 
 const getStorageData = key => {
   return new Promise(resolve => {
@@ -25,7 +26,10 @@ const setStorageData = (key, value) => {
 document.querySelector('#save-button').addEventListener('click', async () => {
   const inputData = document.querySelector('#keyword-list').value;
   const keywords = inputData.split('\n').filter(x => x !== '');
+  const isRegexEnabled = document.querySelector('#regex-switch').checked;
   await setStorageData(storageKey, keywords);
+  await setStorageData(regexKey, isRegexEnabled);
+
   const snackbar = document.querySelector('#save-notifier').MaterialSnackbar;
   snackbar.showSnackbar({
     message: '設定を保存しました',
@@ -34,11 +38,16 @@ document.querySelector('#save-button').addEventListener('click', async () => {
 });
 
 const init = async () => {
-  const storageData = await getStorageData(storageKey);
-  const keywordList = storageData[storageKey];
+  const keywordList = (await getStorageData(storageKey))[storageKey];
+  const isRegexEnabled = (await getStorageData(regexKey))[regexKey];
 
   const textArea = document.querySelector('#keyword-list');
+  const regexSwitch = document.querySelector('#regex-switch');
+
   textArea.value = keywordList.join('\n');
+  if (isRegexEnabled) {
+    regexSwitch.parentElement.MaterialSwitch.on();
+  }
 };
 
 init();
